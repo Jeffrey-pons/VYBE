fe38a080e4e464c7ee667709f6d5c79e0df89485
Jeffrey <ponsjeffrey@gmail.com>
2025-03-22 19:31:49 +0100

    add & setUp api/services for tmdb;openAgenda & Overpass API

---
+++ api/openAgenda.ts
@@ -0,0 +1,104 @@
+import { Event } from "@/interfaces/Event";
+
+export const getEvent = async (): Promise<Event[]> => {
+    return [];
+}
+
+const generateOpenAgendaUrl = (params: { city?: string, category?: string, timings?: { start: string, end: string }, keyword?: string}) => {
+    const baseUrl = process.env.EXPO_PUBLIC_OPEN_AGENDA_API_URL;
+    const apiKey = process.env.EXPO_PUBLIC_OPEN_AGENDA_API_KEY; 
+    const lang = 'fr'; 
+    const relative = 'upcoming'; 
+  
+    let urlOpenAgenda = `${baseUrl}?key=${apiKey}&lang=${lang}&relative[]=${relative}`;
+  
+    if (params.city) urlOpenAgenda += `&city=${params.city}`;
+    if (params.category) urlOpenAgenda += `&search=${params.category}`;
+    if (params.timings) urlOpenAgenda += `&timings[gte]=${params.timings.start}&timings[lte]=${params.timings.end}`;
+    if (params.keyword) urlOpenAgenda += `&keyword[]=${params.keyword}`;
+  
+    return urlOpenAgenda;
+  };
+
+export const getEventsOpenAgenda = async (urlOpenAgenda: string) => {
+    try {
+        const response = await fetch(urlOpenAgenda);
+        if (!response.ok) {
+            throw new Error(`Erreur HTTP: ${response.status}`);
+        }
+        const data = await response.json();
+        if (data?.events) {
+            return data.events;
+        }
+        return [];
+    } catch (error) {
+        console.error(error);
+        throw new Error('Erreur lors de la récupération des événements');
+    }
+}
+
+export const getEventsOpenAgendaByCategory = async (params: { city?: string, category?: string }) => {
+    try {
+        const urlOpenAgenda = generateOpenAgendaUrl(params);
+        return getEventsOpenAgenda(urlOpenAgenda);
+    } catch (error) {
+        console.error(error);
+        throw new Error('Erreur lors de la récupération des événements');
+    }
+}
+
+export const getEventsOpenAgendaTonight = async (city: string) => {
+    try {
+        const now = new Date();
+        const startOfEvening = new Date(now.setHours(18, 0, 0, 0)); // Ce soir à 18h00
+        const endOfEvening = new Date(now.setHours(23, 59, 59, 999)); // Ce soir jusqu'à 23h59
+      
+        const urlOpenAgenda = generateOpenAgendaUrl({
+          city,
+          timings: { start: startOfEvening.toISOString(), end: endOfEvening.toISOString() },
+        });
+        return getEventsOpenAgenda(urlOpenAgenda);
+    } catch (error) {
+        console.error(error);
+        throw new Error('Erreur lors de la récupération des événements');
+    }
+};
+
+export const getEventsOpenAgendaThisWeek = async (city: string) => {
+    try {
+        const today = new Date();
+        const startOfWeek = new Date(today);
+        startOfWeek.setDate(today.getDate() - today.getDay());  
+        
+        const endOfWeek = new Date(today);
+        endOfWeek.setDate(today.getDate() + (6 - today.getDay())); 
+        
+        startOfWeek.setHours(0, 0, 0, 0);
+        endOfWeek.setHours(23, 59, 59, 999);
+      
+        const urlOpenAgenda = generateOpenAgendaUrl({
+          city,
+          timings: { start: startOfWeek.toISOString(), end: endOfWeek.toISOString() },
+        });
+        return getEventsOpenAgenda(urlOpenAgenda);
+    } catch (error) {
+        console.error(error);
+        throw new Error('Erreur lors de la récupération des événements');
+    }
+  };
+
+  export const getFiveUpcomingEventsOpenAgenda = async (filters = {}) => {
+    try {
+        const urlOpenAgenda = generateOpenAgendaUrl(filters);  
+        const response = await fetch(urlOpenAgenda);  
+        const data = await response.json();  
+        if (data && data.events) {
+            return data.events.slice(0, 50);
+        }
+        return [];  
+    } catch (error) {
+        console.error('Erreur lors de la récupération des événements:', error);
+        throw new Error('Erreur lors de la récupération des événements');
+    }
+  };
+  
--- services/eventService.ts
+++ services/eventService.ts
@@ -1,5 +1,20 @@
-import { Event } from "@/interfaces/Event";
+import { getEventsOpenAgenda } from '@/api/openAgenda';
+// import { getEventBriteEvents } from '@/api/eventBrite';
+import { getEventsTmdb } from '@/api/tmdb';
 
-export const getEvent = async (): Promise<Event[]> => {
-    return [];
-}
+export const getEvents = async (city: string) => {
+    try {
+        const openAgendaEvents = await getEventsOpenAgenda(`https://api.openagenda.com/v1/events?city=${city}`);
+        // const eventBriteEvents = await getEventBriteEvents(`https://www.eventbriteapi.com/v3/events/search/?location.address=${city}`);
+        const tmdbEvents = await getEventsTmdb(48.8566, 2.3522);
+
+        return {
+            openAgendaEvents,
+            // eventBriteEvents,
+            tmdbEvents,
+        };
+    } catch (error) {
+        console.error('Erreur lors de la récupération des événements combinés:', error);
+        throw new Error('Erreur lors de la récupération des événements combinés');
+    }
+};
